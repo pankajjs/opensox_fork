007ab013d923dc3ca8c10eeaf56cd847cdf68e20
apsinghdev <ajeetpratap517@gmail.com>
2026-01-06 16:27:16 +0530

    feat: add existing feedbacks

--- apps/web/src/data/testimonials.ts
+++ apps/web/src/data/testimonials.ts
@@ -1,9 +1,9 @@
 // Image testimonials data
 export type ImageTestimonial = {
-    id: string;
-    type: "image";
-    imageUrl: string;
-    alt: string;
+  id: string;
+  type: "image";
+  imageUrl: string;
+  alt: string;
 };
 
 export const imageTestimonials: ImageTestimonial[] = [
@@ -22,10 +22,38 @@ export const imageTestimonials: ImageTestimonial[] = [
     alt: "User feedback about Opensox Pro",
   },
   {
-    id: "img-1",
+    id: "img-3",
     type: "image",
     imageUrl:
       "https://res.cloudinary.com/desjc8vzi/image/upload/v1767695552/Screenshot_2026-01-06_at_4.02.24_PM_ubg5ts.png",
+    alt: "User feedback about Opensox Pro",
+  },
+  {
+    id: "img-4",
+    type: "image",
+    imageUrl:
+      "https://res.cloudinary.com/desjc8vzi/image/upload/v1767696615/Screenshot_2026-01-06_at_4.20.07_PM_viwal4.png",
+    alt: "User feedback about Opensox Pro",
+  },
+  {
+    id: "img-5",
+    type: "image",
+    imageUrl:
+      "https://res.cloudinary.com/desjc8vzi/image/upload/v1767696639/Screenshot_2026-01-06_at_4.20.34_PM_rridt0.png",
+    alt: "User feedback about Opensox Pro",
+  },
+  {
+    id: "img-6",
+    type: "image",
+    imageUrl:
+      "https://res.cloudinary.com/desjc8vzi/image/upload/v1767696717/Screenshot_2026-01-06_at_4.21.49_PM_rf4zld.png",
+    alt: "User feedback about Opensox Pro",
+  },
+  {
+    id: "img-7",
+    type: "image",
+    imageUrl:
+      "https://res.cloudinary.com/desjc8vzi/image/upload/v1767696812/Screenshot_2026-01-06_at_4.23.25_PM_vkcpns.png",
     alt: "User feedback about Opensox Pro",
   },
 ];
